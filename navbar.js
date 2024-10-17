5003f619ed3216cde5e1f431caea983487efb69a
Custom3Dprints <marjorie.3dprints@gmail.com>
2024-10-17 14:59:06 -0400

    navbar fixed

--- navbar.js
+++ navbar.js
@@ -1,27 +1,26 @@
 
 
 //navbar.html in Home.html
+// Fetch navbar.html and insert its content into #navbar
 fetch('navbar.html')
-    .then(response => response.text())
-    .then(html => document.getElementById('navbar').innerHTML = html);
-
-
-function Home() {
-    window.location.href = "Home.html";
-}
-function Menu() {
-    window.location.href = "Menu.html";
-}
-function Reservations() {
-    window.location.href = "Reservations.html";
-}
-function Events() {
-    window.location.href = "Events.html";
-}
-function About() {
-    window.location.href = "About.html";
-}
-function Reviews() {
-    window.location.href = "Reviews.html";
-}
+	.then(response => response.text())
+	.then(html => {
+		document.getElementById('navbar').innerHTML = html;
+		
+		// Attach event listeners
+		document.getElementById('home-link').addEventListener('click', () => window.location.href = "Home.html");
+		document.getElementById('menu-link').addEventListener('click', () => window.location.href = "Menu.html");
+		document.getElementById('reservations-link').addEventListener('click', () => window.location.href = "Reservations.html");
+		document.getElementById('events-link').addEventListener('click', () => window.location.href = "Events.html");
+		document.getElementById('about-link').addEventListener('click', () => window.location.href = "About.html");
+		document.getElementById('reviews-link').addEventListener('click', () => window.location.href = "Reviews.html");
+		
+		// Mobile menu toggle
+		const menu = document.querySelector('#mobile-menu');
+		const menuLinks = document.querySelector('.navbar__menu');
+		menu.addEventListener('click', function(){
+			menu.classList.toggle('is-active');
+			menuLinks.classList.toggle('active');
+		});
+	});
 
